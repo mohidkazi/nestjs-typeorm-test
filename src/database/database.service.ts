898c1a050b04887640bf56a10587526db06e70bf
Mohid Kazi <mohid.kazi@cloudesign.com>
2020-08-05 10:45:02 +0530

    changed database service path

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -4,15 +4,14 @@ import { Client, createConnection } from 'mysql'
 
 @Injectable()
 export class DatabaseService {
-    connection = new Client();
     constructor() {
-        this.connection = createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'mysql123',
-            database: 'fakedb'
-        });
     }
+    connection = createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'mysql123',
+        database: 'fakedb'
+    });
 
     async connect() {
         this.connection.connect()
--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,15 +1,25 @@
+import { IsOptional, IsString, IsUUID, IsNumber } from 'class-validator';
+
 export class AddUserDto {
+    @IsOptional()
+    @IsString()
     name: string;
 
+    @IsOptional()
+    @IsString()
     alias: string;
 }
 
 export class GetUsersDto {
+    @IsUUID()
     id: string
 
+    @IsString()
     name: string;
 
+    @IsString()
     alias: string;
 
-    isDeleted: string | number
+    @IsNumber()
+    isDeleted: number
 }
